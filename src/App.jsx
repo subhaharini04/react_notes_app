0c6be87cb3ab5bd9df75c03f9f10d83bf6a4ac2f
subhaharini04 <subhaharini.act@gmail.com>
2025-08-18 17:58:23 +0530

    added local storage function

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [notes, setNotes] = useState(() => {
+  const saved = localStorage.getItem("notes");
+  return saved ? JSON.parse(saved) : [];
+});
 
-  return (
-    <>
-      <h1>Notes App</h1>
-      <input 
+  useEffect(() => {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}, [notes]);
+
+const addNote = () => {
+  if (title.trim() === "" && content.trim() === "") {
+    alert("Title and content cannot be empty");
+    return;
+  }
+  const newNote = { title, content };
+  setNotes([...notes, newNote]);
+  setTitle("");
+  setContent("");
+  console.log("Note added:", newNote);
+}
+return (
+  <>
+    <h1>Notes App</h1>
+    <input
       type="text"
       value={title}
       onChange={(e) => setTitle(e.target.value)}
       placeholder='Enter title here'
-      />
+    />
 
-      <textarea 
-      value={content} 
-      onChange={(e) => setContent(e.target.value)} 
-      placeholder='Enter content here'/>
+    <textarea
+      value={content}
+      onChange={(e) => setContent(e.target.value)}
+      placeholder='Enter content here' />
 
-    <button>Add Note</button>
-    </>
-  )
+    <button
+      onClick={addNote}>Add Note
+    </button>
+
+    <div>
+      <h2>Note History</h2>
+      {notes.length === 0 ? (
+        <p> No Notes Yet</p>
+      ) : (
+        notes.map((note, index) => (
+          <div key={index}>
+            <h3>{note.title}</h3>
+            <p>{note.content}</p>
+          </div>
+        ))
+      )}
+    </div>
+  </>
+);
 }
-
 export default App
